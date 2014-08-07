bb03ec83d6d4a315455495fbbdd5544fa8025c1b
applenick <applenick3@me.com>
2014-08-06 17:22:16 -0700

    Use an ArrayList instead

--- src/main/java/com/applenick/iWardrobe/listeners/InventoryListener.java
+++ src/main/java/com/applenick/iWardrobe/listeners/InventoryListener.java
@@ -1,5 +1,8 @@
 package com.applenick.iWardrobe.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -14,7 +17,10 @@ public class InventoryListener implements Listener {
 	
 	@EventHandler
 	public void onArmorClick(InventoryClickEvent event){
-		if(event.getSlotType().equals(SlotType.ARMOR) && event.getCurrentItem().getItemMeta().getLore().toString().equalsIgnoreCase(iWardrobe.wardrobe_lore)){
+		List<String> lore = new ArrayList<String>();
+		lore.add(iWardrobe.wardrobe_lore);
+		
+		if(event.getSlotType().equals(SlotType.ARMOR) && event.getCurrentItem().getItemMeta().getLore().equals(lore)){
 			Player p = (Player) event.getWhoClicked();
 			p.closeInventory();
 			p.sendMessage(ChatColor.RED + "✕" +  ChatColor.GOLD +" You Are Not Allowed to interact with Wardrobe Items");
