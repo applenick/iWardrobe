3104042c895aa9616ad72dd94705d1b083eae6cc
applenick <applenick3@me.com>
2014-08-14 12:37:19 -0700

    Add Lore to Leggings

--- src/main/java/com/applenick/iWardrobe/inventory/submenus/PantsMenu.java
+++ src/main/java/com/applenick/iWardrobe/inventory/submenus/PantsMenu.java
@@ -1,5 +1,6 @@
 package com.applenick.iWardrobe.inventory.submenus;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -11,8 +12,10 @@ import net.njay.annotation.MenuInventory;
 import net.njay.annotation.MenuItem;
 
 import com.applenick.iWardrobe.WardrobePlayer;
+import com.applenick.iWardrobe.iWardrobe;
 import com.applenick.iWardrobe.inventory.WardrobeMenu;
 import com.applenick.iWardrobe.sounds.SoundUtil;
+import com.applenick.iWardrobe.utils.ItemUtils;
 
 
 @MenuInventory(slots = 9 ,
@@ -32,7 +35,7 @@ public class PantsMenu extends Menu {
 					lore = "§6Top of the line")
 			)
 	public void diamondLegs(WardrobePlayer player){
-		player.getBukkit().getInventory().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+		player.getBukkit().getInventory().setLeggings(ItemUtils.getLoreItem(Material.DIAMOND_LEGGINGS, ChatColor.AQUA + "Diamond Leggings", iWardrobe.wardrobe_lore));
 		player.getBukkit().closeInventory();
 		SoundUtil.click(player.getBukkit());
 	}
@@ -44,7 +47,7 @@ public class PantsMenu extends Menu {
 					lore = "§6For the stylish one")
 			)
 	public void goldenLegs(WardrobePlayer player){
-		player.getBukkit().getInventory().setLeggings(new ItemStack(Material.GOLD_LEGGINGS));
+		player.getBukkit().getInventory().setLeggings(ItemUtils.getLoreItem(Material.GOLD_LEGGINGS, ChatColor.YELLOW + "Gold Leggings", iWardrobe.wardrobe_lore));
 		player.getBukkit().closeInventory();
 		SoundUtil.click(player.getBukkit());
 	}
@@ -56,7 +59,7 @@ public class PantsMenu extends Menu {
 					lore = "§6Strong yet simple")
 			)
 	public void ironLegs(WardrobePlayer player){
-		player.getBukkit().getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+		player.getBukkit().getInventory().setLeggings(ItemUtils.getLoreItem(Material.IRON_LEGGINGS, ChatColor.GRAY + "Iron Leggings", iWardrobe.wardrobe_lore));
 		player.getBukkit().closeInventory();
 		SoundUtil.click(player.getBukkit());
 	}
@@ -68,7 +71,7 @@ public class PantsMenu extends Menu {
 					lore = "§6Hidden beauty")
 			)
 	public void chainmailLegs(WardrobePlayer player){
-		player.getBukkit().getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+		player.getBukkit().getInventory().setLeggings(ItemUtils.getLoreItem(Material.CHAINMAIL_LEGGINGS, ChatColor.RED + "Chainmail Leggings", iWardrobe.wardrobe_lore));
 		player.getBukkit().closeInventory();
 		SoundUtil.click(player.getBukkit());
 	}
@@ -80,7 +83,7 @@ public class PantsMenu extends Menu {
 					lore = "§6For the average ones")
 			)
 	public void leatherLegs(WardrobePlayer player){
-		player.getBukkit().getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+		player.getBukkit().getInventory().setLeggings(ItemUtils.getLoreItem(Material.LEATHER_LEGGINGS, ChatColor.LIGHT_PURPLE + "Leather Leggings", iWardrobe.wardrobe_lore));
 		player.getBukkit().closeInventory();
 		SoundUtil.click(player.getBukkit());
 	}
