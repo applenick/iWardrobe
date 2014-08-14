fe827425da839dc6c64f066701e69d1703b8f4cb
applenick <applenick3@me.com>
2014-08-14 15:05:01 -0700

    Create Easier Access to Wardrobe SubMenus

--- src/main/java/com/applenick/iWardrobe/commands/WardrobeCommands.java
+++ src/main/java/com/applenick/iWardrobe/commands/WardrobeCommands.java
@@ -8,8 +8,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.applenick.iWardrobe.WardrobePlayer;
+import com.applenick.iWardrobe.inventory.ChoiceMenu;
 import com.applenick.iWardrobe.inventory.WardrobeMenu;
+import com.applenick.iWardrobe.inventory.submenus.BootsMenu;
+import com.applenick.iWardrobe.inventory.submenus.ChestPlateMenu;
 import com.applenick.iWardrobe.inventory.submenus.HatMenu;
+import com.applenick.iWardrobe.inventory.submenus.PantsMenu;
 import com.applenick.iWardrobe.utils.CommandUtil;
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
@@ -20,6 +24,7 @@ public class WardrobeCommands {
 	@Command(
 			aliases = { "wardrobe", "wd" },
 			desc = "Opens your Wardrobe",
+			usage = "<hats | chestplate | leggings | boots| reset>",
 			min = 0,
 			max = 1
 			)
@@ -33,8 +38,26 @@ public class WardrobeCommands {
 
 		
 		if(args.argsLength() == 1){
-			if(args.getJoinedStrings(0) == "hats"){
+			if(args.getString(0).equalsIgnoreCase("hats")){
 				player.setActiveMenu(new HatMenu(player.getMenuManager() , null));
+			}
+			
+			if(args.getString(0).equalsIgnoreCase("chestplate")){
+				player.setActiveMenu(new ChestPlateMenu(player.getMenuManager() , null));
+			}
+			
+			if(args.getString(0).equalsIgnoreCase("leggings")){
+				player.setActiveMenu(new PantsMenu(player.getMenuManager() , null));
+				
+			}
+			
+			if(args.getString(0).equalsIgnoreCase("boots")){
+				player.setActiveMenu(new BootsMenu(player.getMenuManager() , null));
+				
+			}
+			
+			if(args.getString(0).equalsIgnoreCase("reset")){
+				player.setActiveMenu(new ChoiceMenu(player.getMenuManager() , null));
 			}
 		}
 		else {
